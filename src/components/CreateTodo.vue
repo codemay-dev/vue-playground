28feb06669c82811dc37a4ab8b86d2c7f124a230
Jaime May <jaime@codemay.com>
2021-01-29 21:33:24 -0800

    Added create todo form card

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -1,4 +1,43 @@
-<template></template>
+<template>
+  <div class="col-12">
+    <button class="btn btn-success" v-on:click="openForm" v-show="!isCreating">
+      Add Todo
+    </button>
+    <div class="col mb-4">
+      <div class="card h-100">
+        <div class="card-body" v-show="isCreating">
+          <div class="mb-3 text-start">
+            <label class="form-label">Title</label>
+            <input
+              class="form-control"
+              type="text"
+              v-model="titleText"
+              ref="title"
+              defaultValue=""
+            />
+          </div>
+          <div class="mb-3 text-start">
+            <label class="form-label">Project</label>
+            <input
+              class="form-control"
+              type="text"
+              ref="project"
+              defaultValue=""
+            />
+          </div>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary mx-1" v-on:click="sendForm()">
+            Create
+          </button>
+          <button class="btn btn-danger mx-1" v-on:click="closeForm">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 </script>
